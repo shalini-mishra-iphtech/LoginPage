70bed194d8e1514ef36b9ba2b1e0ef438e2b47d4
Shalini Mishra <shalini.mishra@iphtechnologies.com>
2025-03-05 11:35:20 +0530

    adding dependecies and thems

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
-
+    implementation ("com.google.android.material:material:1.12.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
